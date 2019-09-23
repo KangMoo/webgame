7f98bfefa3084757e8a59e84d0a2bc9757f094e6
sah0928 <sah0928@naver.com>
2019-09-23 22:53:26 +0900

    AWS RDS 세팅
    DB info 설정

--- PhaserGame/app.js
+++ PhaserGame/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,7 +31,6 @@ app.use(express.static('./games/FlappyBird'));
 app.use(express.static('./games/SocketCommunicationTest'));
 app.use(express.static('./games/heeje'));
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -46,5 +46,24 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// db connect
+var connection = mysql.createConnection({
+  host     : 'web-database.ct82p2wdqupw.ap-northeast-2.rds.amazonaws.com',
+  user     : 'root',
+  password : 'test123!',
+  port     : '3306'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
 
 module.exports = app;
